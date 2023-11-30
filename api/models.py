aec2cd7f6d298b3808f8e4fc48170b6883cccb04
sandhya <sandhya.201637@ncit.edu.np>
2023-11-30 17:20:00 +0545

    UCM

--- api/models.py
+++ api/models.py
@@ -3,12 +3,28 @@ from django.db import models
 from django.forms import ModelForm
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-class  users(models.Model):
-    uid= models.BigAutoField(primary_key=true)
+class  Users(models.Model):
+    u_id= models.BigAutoField(primary_key=true)
+    role_id= models.IntegerField(foreign_key=true)
     uname= models.CharField(max_length=100)
     email = models.EmailField(max_length=100, unique=True)
     date_joined = models.DateTimeField(auto_now_add=True)
-   
 
-    REQUIRED_FIELDS = ["uname", "email"]
+class Role(models.Model):
+    role_id=models.AutoField(primary_key=true)
+    role==models.CharField(max_length=100)
 
+class Content(models.Model):
+    content_id=models.BigAutoField(primary_key=true)
+    content_jason=models.JSONField()
+    file_id=models.IntegerField(foreign_key=true)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class File(models.Model):
+    file_id=models.BigAutoField(primary_key=true)
+    path=models.CharField(max_length=250)
+
+class Class(models.Model):
+    class_id=models.BigAutoField(primary_key=true)
+    subject_name=models.CharField(max_length=250)
+    semister= models.CharField(max_length=100)
