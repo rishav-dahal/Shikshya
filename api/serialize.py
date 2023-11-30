5e33cc24b920a6be4757a8a8df51537885bf5e09
Rishav Dahal <82385662+rishav-dahal@users.noreply.github.com>
2023-11-30 17:36:52 +0545

    response
    serialization , views and its url configure completed for data retrival

---
+++ api/serialize.py
@@ -0,0 +1,38 @@
+from rest_framework import serializers
+class UserSerializer(serializers.Serializer):
+    u_id = serializers.IntegerField
+    role_id = serializers.IntegerField()
+    u_name = serializers.CharField(max_length=10)
+    u_email =serializers.EmailField()
+    hash = serializers.CharField(max_length=100)
+    verification = serializers.CharField(max_length=100)
+    created_at = serializers.DateTimeField()
+    
+
+class RoleSerializer(serializers.Serializer):
+    role_id = serializers.IntegerField()
+    role_name = serializers.CharField()
+
+class ContentSerializer(serializers.Serializer):
+    content_id = serializers.IntegerField()
+    content = serializers.CharField()
+    file_id = serializers.CharField()
+    created_at = serializers.DateTimeField
+    u_id = serializers.IntegerField
+    class_id = serializers.IntegerField
+    title = serializers.CharField
+    
+class FileSerializer(serializers.Serializer):
+    file_id = serializers.CharField
+    path = serializers.CharField
+    u_id = serializers.IntegerField
+    class_id = serializers.IntegerField
+ 
+class ClassSerializer(serializers.Serializer):
+    class_id = serializers.IntegerField
+    subject_name = serializers.CharField
+    semester = serializers.IntegerField
+
+        
+
+    
