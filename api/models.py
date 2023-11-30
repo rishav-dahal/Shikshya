8c915f0b46daf70f0b3d9326739e1c6c8639044c
sandhya <sandhya.201637@ncit.edu.np>
2023-11-30 16:45:49 +0545

    data model added

--- api/models.py
+++ api/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+from django.forms import ModelForm
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+
+class  users(models.Model):
+    uid= models.BigAutoField(primary_key=true)
+    uname= models.CharField(max_length=100)
+    email = models.EmailField(max_length=100, unique=True)
+    date_joined = models.DateTimeField(auto_now_add=True)
+   
+
+    REQUIRED_FIELDS = ["uname", "email"]
+
